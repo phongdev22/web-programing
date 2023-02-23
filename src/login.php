8a298d5feb5198a21ac646a4819e1e62a28662a0
nguyenphong0803 <nguyenphong22803@gmail.com>
2023-02-23 14:57:56 +0700

    upload login.php

--- src/login.php
+++ src/login.php
@@ -6,22 +6,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>
-    <link rel="stylesheet" href="../assets/styles/style.css" />
+    <!-- <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
+      <script
+         src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"
+         type="text/javascript"
+      ></script> -->
+    <link rel="stylesheet" href="../src/assets/styles/style.css" />
 </head>
 
 <body>
     <div class="wrapper">
-        <form id="login-form" method="POST">
+        <form id="login-form" action="./view/login.php" method="POST">
             <h1 id="login-heading">Login</h1>
             <div class="form-group">
                 <label for="user" class="login-label text">User name</label>
-                <input type="text" id="user" class="login-input" value="<?php echo $_POST["user"] ?>" name="user" autocomplete="" />
+                <input type="text" id="user" class="login-input" name="user" value="<?php echo $_POST['user'] ?>" />
             </div>
             <div class="form-group">
                 <label for="password" class="login-label text">Password</label>
-                <input type="password" id="password" class="login-input" value="<?php echo $_POST["pwd"] ?>" name="pwd" autocomplete="" />
+                <input type="password" id="password" class="login-input" name="pwd" value="<?php echo $_POST['pwd'] ?>" />
             </div>
-            <div class="form-group">
+            <div class=" form-group">
                 <a href="forgetp.php" class="forgot-password text">Forgot password!</a>
             </div>
             <div class="form-group">
--- src/view/index.php
+++ src/view/index.php
@@ -1,3 +1,13 @@
 <?php
-
-echo "Login successfully!";
+$user;
+$pwd;
+if ($_POST) {
+    $GLOBALS["user"] = $_POST['user'];
+    $GLOBALS["pwd"] = $_POST['pwd'];
+    if ($user == "admin" && $pwd == "1234") {
+        include "/xampp/htdocs/web/src/view/index.php";
+        die();
+    } else {
+        echo "111";
+    }
+}
